3b397696138ebeecd11dc119f02fbd960875f6c2
Nepcam <cameron.nepe@gmail.com>
2019-07-09 19:45:06 +1200

    loadCustomer function, xhr.open, xhr.onload, customer.json

---
+++ app.js
@@ -0,0 +1,17 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+
+function loadCustomer(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json', true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      // console.log(this.responseText)
+
+      const customer = JSON.parse
+    }
+  }
+
+  xhr.send();
+}
