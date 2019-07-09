ba0e6fb727348333f727a9004f874f9be8a298cb
Nepcam <cameron.nepe@gmail.com>
2019-07-09 20:11:53 +1200

    added a function for the customers button

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 document.getElementById('button1').addEventListener('click', loadCustomer);
 
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+// Load single customer
 function loadCustomer(e) {
   const xhr = new XMLHttpRequest();
 
@@ -9,7 +12,50 @@ function loadCustomer(e) {
     if(this.status === 200) {
       // console.log(this.responseText)
 
-      const customer = JSON.parse
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+        `;
+
+      document.getElementById('customer').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+}
+
+// Load customers
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      // console.log(this.responseText)
+
+      const customers = JSON.parse(this.responseText);
+
+      let output = '';
+
+      customers.forEach(function(customer) {
+        output += `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Company: ${customer.company}</li>
+          <li>Phone: ${customer.phone}</li>
+        </ul>
+        `;
+      });
+
+      document.getElementById('customers').innerHTML = output;
     }
   }
 
